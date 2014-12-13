79cc11ca6d0ecad1987fd744da70107d87fb7ef1
David Minnerly <dtreeLive@hotmail.com>
2014-12-13 13:11:39 -0500

    Remove Cure-specific functionality

--- build.lua
+++ build.lua
@@ -37,15 +37,6 @@ local IGNORED = {
 }
 
 --[[
-  Array of alternative paths to output the contents of the model. You must
-  specify the full file path and extension.
---]]
-local LOCATIONS = {
-  -- "output.rbxmx",
-  -- "test/game.rbxmx"
-}
-
---[[
   Where source code is stored and compiled to, respectively.
 --]]
 local SOURCE_DIR = "source"
@@ -58,7 +49,7 @@ local BUILD_DIR  = "build"
       binary while the latter uses XML. Because this build only compiles to
       XML, the rbxmx file extension is prefered.
 --]]
-local RBXM_FILE_NAME = "cure"
+local RBXM_FILE_NAME = "elixir"
 local RBXM_FILE_EXT  = ".rbxmx" -- [1]
 local RBXM_FILE = RBXM_FILE_NAME..RBXM_FILE_EXT
 
@@ -66,16 +57,13 @@ local RBXM_FILE = RBXM_FILE_NAME..RBXM_FILE_EXT
   Sets the name for the top-most instance in the model file. This contains all
   of the descendants of the source directory, once they're compiled.
 --]]
-local RBXM_ROOT_NAME  = "cure"
+local RBXM_ROOT_NAME  = "elixir"
 
 --[[
   The instance that will be used to replicate the folder structure. Any
   instance can be used, but Folders are recommended.
 --]]
 local CONTAINER_CLASS = "Configuration"
-
--- maximum length of strings in replicated instances
-local MAX_STRING_LENGTH = 200000 - 1
 
 
 
@@ -323,86 +311,6 @@ function encode.ProtectedString(data)
   return xml:encodeTruncEsc(xml:escape(data))
 end
 
-function encode.CoordinateFrame(data)
-  local d = { data:components() }
-
-  return xml:neW()
-    :ln():ind(1):append(("<X>"..d[1].."</X>"))
-    :ln():ind(1):append(("<Y>"..d[2].."</Y>"))
-    :ln():ind(1):append(("<Z>"..d[3].."</Z>"))
-    :ln():ind(1):append(("<R00>"..d[4].."</R00>"))
-    :ln():ind(1):append(("<R01>"..d[5].."</R01>"))
-    :ln():ind(1):append(("<R02>"..d[6].."</R02>"))
-    :ln():ind(1):append(("<R10>"..d[7].."</R10>"))
-    :ln():ind(1):append(("<R11>"..d[8].."</R11>"))
-    :ln():ind(1):append(("<R12>"..d[9].."</R12>"))
-    :ln():ind(1):append(("<R20>"..d[10].."</R20>"))
-    :ln():ind(1):append(("<R21>"..d[11].."</R21>"))
-    :ln():ind(1):append(("<R22>"..d[12].."</R22>"))
-    :save()
-end
-
-function encode.Color3(data)
-  return tonumber(string.format("0xFF%02X%02X%02X", data.r*255, data.g*255, data.b*255))
-end
-
-function encode.Content(data)
-  if #data == 0 then
-    return "<null></null>"
-  else
-    return { "<url>"..data.."</url>" }
-  end
-end
-
-function encode.Ray(data)
-  local o = data.Origin
-  local d = data.Direction
-  return xml:new()
-    :ln():ind(1):append("<origin>")
-    :ln():ind(2):append("<X>", o.x, "</X>")
-    :ln():ind(2):append("<Y>", o.y, "</Y>")
-    :ln():ind(2):append("<Z>", o.z, "</Z>")
-    :ln():ind(1):append("</origin>")
-    :ln():ind(1):append("<direction>")
-    :ln():ind(2):append("<X>", d.x, "</x>")
-    :ln():ind(2):append("<Y>", d.y, "</Y>")
-    :ln():ind(2):append("<Z>", d.z, "</Z>")
-    :ln():ind(1):append("</direction>")
-    :save()
-end
-
-function encode.Vector3(data)
-  return xml:new()
-    :ln():ind(1):append("<X>"..data.x.."</X>")
-    :ln():ind(1):append("<Y>"..data.y.."</Y>")
-    :ln():ind(1):append("<Z>"..data.z.."</Z>")
-    :save()
-end
-
-function encode.Vector2(data)
-  return xml:new()
-    :ln():ind(1):append("<X>"..data.x.."</X>")
-    :ln():ind(1):append("<Y>"..data.y.."</Y>")
-    :save()
-end
-
-function encode.UDim2()
-  return xml:new()
-    :ln():ind(1):append("<XS>"..data.X.Scale.."</XS>")
-    :ln():ind(1):append("<XO>"..data.X.Offset.."</XO>")
-    :ln():ind(1):append("<YS>"..data.Y.Scale.."</YS>")
-    :ln():ind(1):append("<YO>"..data.Y.Offset.."</YO>")
-    :save()
-end
-
-function encode.Ref(data)
-  if data == nil then
-    return "null"
-  else
-    return data
-  end
-end
-
 
 
 
@@ -462,54 +370,6 @@ end
 
 function rbxm:createLocalScript(...)
   return self:createScript("LocalScript", ...)
-end
-
---[[
-  Create an IntValue containing the ID of a Roblox asset. Things like Models,
-  Decals and T-Shirts are all assets, and you can find their ID at the end of
-  the URL.
-
-  @param string name  Name of the value
-  @param number value ID of a Roblox asset. The number at the end of the URL on
-                      an item. Eg. 42891177, 40469899, 39053953
---]]
-function rbxm:createAsset(file, name, value)
-  value = tonumber(value)
-  if not value then
-    print("WARNING: content of '"..file.."' must be a number")
-  end
-  return self:createValue("Int", name, value)
-end
-
---[[
-  Split apart the contents of the file into multiple StringValues, contained
-  inside a BoolValue.
-
-  Example (varies, depending on size):
-
-    extremely-large-file.lua
-
-  Turns into:
-
-    extremely-large-file
-    - 1
-    - 2
-    - 3
-    - etc.
---]]
-function rbxm:splitFileParts(content)
-  local chunk = MAX_STRING_LENGTH
-  local length = #content
-  local container = rbxm:createValue("Bool", name, true)
-
-  for i = 1, math.ceil(length/chunk) do
-    local a = (i - 1)*chunk + 1
-    local b = a + chunk - 1
-    b = b > length and length or b
-    container[i] = rbxm:createValue("String", tostring(i), content:sub(a, b))
-  end
-
-  return container
 end
 
 --[[
@@ -668,14 +528,11 @@ end
 --[[
   Run functions for specific types of files.
 
-  @param string path       Full path to the current file. LFS needs this to read
-                           the file.
-  @param string file       Name and extension of the file.
-  @param bool   subfolder  Cure's scripts live in the root of the source dir, if
-                           'subfolder' is true it's safe to assume that the
-                           following scripts belong to Cure.
+  @param string path Full path to the current file. LFS needs this to read the
+                     file.
+  @param string file Name and extension of the file.
 --]]
-local function handleFile(path, file, subfolder)
+local function handleFile(path, file)
   local content = getFileContents(path)
   local name, extension = splitName(file)
   local subName, subExtension = splitName(name)
@@ -683,46 +540,17 @@ local function handleFile(path, file, subfolder)
   extension = extension:lower()
   subExtension = subExtension:lower()
 
-  -- Special handling for the main Cure scripts
-  if not subfolder then
-    rbxm:checkScriptSyntax(content)
-
-    if file:lower() == "cure.server.lua" then
-      return rbxm:createServerScript("cure.server", content)
-    elseif file:lower() == "cure.client.lua" then
-      return rbxm:createLocalScript("cure.client", content)
-    end
-  end
-
   if extension == "lua" then
     rbxm:checkScriptSyntax(content)
-
     if subExtension == "script" then
       return rbxm:createServerScript(subName, content)
     elseif subExtension == "localscript" then
       return rbxm:createLocalScript(subName, content)
-    else
-      local chunk = MAX_STRING_LENGTH
-      local length = #content
-
-      if length <= chunk then
-        -- Create a StringValue to hold the source of the file
-        return rbxm:createValue("String", name, content)
-      else
-        -- If the file is too big, split it into multiple parts
-        return rbxm:splitFileParts(content)
-      end
     end
-  elseif extension == "asset" then
-    -- Create an IntValue containing a Roblox AssetID
-    return rbxm:createAsset(file, name, content)
-  else
-    -- Disable and comment out anything else
-    return rbxm:createServerScript(name, "--[==[\n"..content.."\n--]==]", true)
   end
 end
 
-local function recurseDir(path, obj, recurse)
+local function recurseDir(path, obj)
   print("DIR", path)
   for name in lfs.dir(path) do
     if isNotIgnored(name) then
@@ -737,7 +565,7 @@ local function recurseDir(path, obj, recurse)
         obj[#obj+1] = recurseDir(joined, dir, true)
       else
         print("FILE", joined)
-        obj[#obj+1] = handleFile(joined, name, recurse)
+        obj[#obj+1] = handleFile(joined, name)
       end
     end
   end
@@ -762,10 +590,6 @@ function compile(args)
 
   lfs.mkdir(BUILD_DIR)
   rbxm:save(rbxmObj, rbxmPath)
-
-  for i,v in ipairs(LOCATIONS) do
-    rbxm:save(rbxmObj, LOCATIONS[i])
-  end
 end
 
 compile({...})
