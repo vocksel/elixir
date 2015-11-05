3d3d38e4d256b5854a61c5ebf309f987f4f566c7
David Minnerly <dtreeLive@hotmail.com>
2015-11-05 14:10:14 -0500

    Fix incorrect version number
    0.1.0 was when I was testing things outside this repository. I forgot to
    change this to the current version of Elixir.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="elixir",
-    version="0.1.0",
+    version="1.1.0",
     description="Turn directories and Lua files into a ROBLOX compatible XML file.",
     author="David Minnerly",
     license="MIT",
