e96490a4e1e509cb06d02028d882bdb55d408366
David Minnerly <dtreeLive@hotmail.com>
2015-12-16 09:21:26 -0500

    Give sample-project a model_name.

--- examples/sample-project/build.py
+++ examples/sample-project/build.py
@@ -4,6 +4,7 @@ from elixir.compiler import ModelCompiler
 
 source = "src/"
 dest = "model.rbxmx"
+model_name = "SampleProject"
 
-compiler = ModelCompiler(source, dest)
+compiler = ModelCompiler(source, dest, model_name=model_name)
 compiler.compile()
