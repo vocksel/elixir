41cc8a5193f7ac65b8a3d967d418467207f9ec8e
David Minnerly <dtreeLive@hotmail.com>
2016-04-25 13:30:41 -0400

    Add a comment about why we set the class in the attrib dict

--- elixir/rbx.py
+++ elixir/rbx.py
@@ -31,6 +31,8 @@ def create_item(class_name):
     # ROBLOX imports models just fine without them, so it's not necessary to
     # include.
     item = ElementTree.Element("Item", attrib={
+        # `class` is a reserved keyword so we have to pass it in through this
+        # dict rather than as a named parameter.
         "class": class_name
     })
 
